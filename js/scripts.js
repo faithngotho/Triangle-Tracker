f7c4ad18d09f205d145c0f47e7dcb519315e9043
faithngotho <faithwangari248@gmail.com>
2019-03-26 15:14:50 +0300

    Declare the variable height.

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,5 @@ function triangle(){
 
   var length = parseFloat(document.getElementById("length"));
   var width = parseFloat(document.getElementById("width"));
+  var height = parseFloat(document.getElementById("height"));
 }
