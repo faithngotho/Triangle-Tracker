c7287312f79857efbe5572a771088e58186cf086
faithngotho <faithwangari248@gmail.com>
2019-03-26 15:17:55 +0300

    Add an if statement to determine if the user has entered a value that is not a number or zero.

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,9 @@ function triangle(){
   var length = parseFloat(document.getElementById("length"));
   var width = parseFloat(document.getElementById("width"));
   var height = parseFloat(document.getElementById("height"));
+
+
+    if(isNan(length,width,height)===true || (length<=0 || width<=0 || height<=0)){
+      output.innerHTML=("Enter a valid number.");
+    }
 }
