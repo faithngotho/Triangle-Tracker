4523679fb03f8d2445bb459a04f87d023a7ba85f
faithngotho <faithwangari248@gmail.com>
2019-03-26 17:18:18 +0300

    Add the background image.

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,26 @@
 function triangle(){
+  var length = parseFloat(document.getElementById("length").value);
+  var width = parseFloat(document.getElementById("width").value);
+  var height = parseFloat(document.getElementById("height").value);
 
-  var length = parseFloat(document.getElementById("length"));
-  var width = parseFloat(document.getElementById("width"));
-  var height = parseFloat(document.getElementById("height"));
 
-
-    if(isNan(length,width,height)===true || (length<=0 || width<=0 || height<=0)){
+    if(isNaN(length,width,height)===true || (length<=0 || width<=0 || height<=0)){
       output.innerHTML=("Enter a valid number.");
     }
     else if (length===width && width===height){
-    output.innerHTML=("This is an equilateral triangle.");
+      output.innerHTML=("This is an equilateral triangle.");
     }
    else if(length===width || width===height || length===height){
-   output.innerHTML=("This is an Isosceles triangle.");
+     output.innerHTML=("This is an Isosceles triangle.");
     }
-    else if((length+width)<=height || (length+height)<=width || (width+height)<length)){
-    output.innerHTML=("The values cannot form a triangle.");
+    else if((length+width)<=height || (length+height)<=width || (width+height)<length){
+      output.innerHTML=("The values cannot form a triangle.");
     }
     else if(length!==width && width!==height){
-    output.innerHTML=("This is a scalene triangle.");
+      output.innerHTML=("This is a scalene triangle.");
     }
 }
 
-function reset()
-{
+function reset(){
   location.reload();
 }
